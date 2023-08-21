353284d2c2b757bf7af6704992b45610e870089d
P34RL <undeadgamer279@gmail.com>
2023-08-21 02:37:48 -0400

    Rocket and Askama

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,33 @@
-use enigo::{Enigo, Key, KeyboardControllable};
-// use std::{thread, time::Duration};
+#[macro_use]
+extern crate rocket;
+use askama::Template;
 
-fn main() {
-    let mut enigo = Enigo::new();
+use rocket::{fs::NamedFile, response::content::RawHtml};
+use std::path::Path;
 
-    enigo.key_click(Key::Layout('='));
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate {
+    title: String,
+    text: String,
+}
+
+#[get("/")]
+fn index() -> RawHtml<String> {
+    let title = String::from("Index");
+    let text = String::from("Hello, from Rust!");
+    let rendered_template = IndexTemplate { title, text }
+        .render()
+        .expect("template rendering");
+    RawHtml(rendered_template)
+}
+
+#[get("/index.js")]
+async fn js() -> Option<NamedFile> {
+    NamedFile::open(Path::new("templates/index.js")).await.ok()
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, js])
 }
