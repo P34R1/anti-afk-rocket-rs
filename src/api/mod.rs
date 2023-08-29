872d2e82826c382fb1c735b764f9a77355d2f80a
P34RL <undeadgamer279@gmail.com>
2023-08-29 18:15:18 -0400

    remove presskey

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,11 +1,6 @@
 pub(crate) mod key_handler;
-pub mod presskey;
-pub mod repeatkey;
+mod repeatkey;
 
 pub fn get_routes() -> std::vec::Vec<rocket::Route> {
-    routes![
-        presskey::press_key,
-        repeatkey::start_repeating,
-        repeatkey::stop_repeating
-    ]
+    routes![repeatkey::start_repeating, repeatkey::stop_repeating]
 }
