5447d7a7eaecc76d76e9c51bf06ca74b6933f0bb
P34RL <undeadgamer279@gmail.com>
2023-09-09 22:39:45 -0400

    rearranged static

--- src/setup/static_files.rs
+++ src/setup/static_files.rs
@@ -1,20 +1,26 @@
 use rocket::response::content::{RawCss, RawJavaScript};
 
+macro_rules! include_static {
+    ($file:expr) => {
+        include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/static/", $file))
+    };
+}
+
 #[get("/favicon.ico")]
 pub fn icon() -> &'static [u8] {
-    include_bytes!("static/favicon.ico")
+    include_static!("favicon.ico")
 }
 
 #[get("/style.css")]
-pub fn css() -> RawCss<&'static str> {
-    RawCss(include_str!("static/style.css"))
+pub fn css() -> RawCss<&'static [u8]> {
+    RawCss(include_static!("style.css"))
 }
 
 #[get("/htmx.min.js")]
-pub fn js() -> RawJavaScript<&'static str> {
-    RawJavaScript(include_str!("static/htmx.min.js"))
+pub fn js() -> RawJavaScript<&'static [u8]> {
+    RawJavaScript(include_static!("htmx.min.js"))
 }
 
-pub fn get_routes() -> Vec<rocket::Route> {
+pub fn get_static_routes() -> Vec<rocket::Route> {
     routes![icon, css, js]
 }
