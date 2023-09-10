8a1c77d46f16fee065a22da8a117baf9fb743c80
P34RL <undeadgamer279@gmail.com>
2023-09-09 22:57:16 -0400

    fixed filename

--- src/setup/mod.rs
+++ src/setup/mod.rs
@@ -1,7 +1,7 @@
-mod figments;
+mod figment;
 mod static_files;
 mod templates;
 
-pub use figments::get_figment;
+pub use figment::get_figment;
 pub use static_files::get_static_routes;
 pub use templates::templates;
