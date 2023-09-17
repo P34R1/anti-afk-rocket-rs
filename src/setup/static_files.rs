e806db98a50a8e3921bd50cfa0302872f006a310
P34RL <undeadgamer279@gmail.com>
2023-09-16 20:52:22 -0400

    removed pub

--- src/setup/static_files.rs
+++ src/setup/static_files.rs
@@ -7,17 +7,17 @@ macro_rules! include_static {
 }
 
 #[get("/favicon.ico")]
-pub fn icon() -> &'static [u8] {
+fn icon() -> &'static [u8] {
     include_static!("favicon.ico")
 }
 
 #[get("/style.css")]
-pub fn css() -> RawCss<&'static [u8]> {
+fn css() -> RawCss<&'static [u8]> {
     RawCss(include_static!("style.css"))
 }
 
 #[get("/htmx.min.js")]
-pub fn js() -> RawJavaScript<&'static [u8]> {
+fn js() -> RawJavaScript<&'static [u8]> {
     RawJavaScript(include_static!("htmx.min.js"))
 }
 
