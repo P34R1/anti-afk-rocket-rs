fcdb3de50dce2e5685db233f323e29e9705e548e
P34RL <undeadgamer279@gmail.com>
2023-09-16 20:55:20 -0400

    made code more readible

--- src/setup/templates.rs
+++ src/setup/templates.rs
@@ -1,37 +1,37 @@
-type RawTemplate<'a> = (&'a str, &'a str);
-
-macro_rules! parse_extensions {
-    ($filename:expr) => {{
-        let parts: Vec<&str> = $filename.split('.').collect();
-
-        &parts[0..parts.len() - 2].join(".")
-    }};
-}
-
-macro_rules! include_template {
-    ($filename:expr) => {
-        (
-            parse_extensions!($filename),
-            include_str!(concat!(
-                env!("CARGO_MANIFEST_DIR"),
-                "/templates/",
-                $filename
-            )),
+macro_rules! template_location {
+    ($filename:literal, $extention:ident) => {
+        concat!(
+            env!("CARGO_MANIFEST_DIR"),
+            "/templates/",
+            $filename,
+            ".",
+            stringify!($extention),
+            ".tera"
         )
     };
 }
+
+macro_rules! include_templates {
+    ($($filename:literal)+, $extention:ident) => {[
+        $((
+            $filename,
+            include_str!(template_location!($filename, $extention)),
+        ),)*
+    ]};
+}
+
+type RawTemplate = (&'static str, &'static str);
+type RawTemplates = [RawTemplate; 2];
+
+const RAW_TEMPLATES: RawTemplates = include_templates!("index" "repeat-key", html);
+
 pub fn templates(engines: &mut rocket_dyn_templates::Engines) {
     if cfg!(debug_assertions) {
         return;
     }
 
-    let raw_templates: [RawTemplate; 2] = [
-        include_template!("index.html.tera"),
-        include_template!("repeat-key.html.tera"),
-    ];
-
     engines
         .tera
-        .add_raw_templates(raw_templates)
+        .add_raw_templates(RAW_TEMPLATES)
         .expect("valid Tera templates");
 }
