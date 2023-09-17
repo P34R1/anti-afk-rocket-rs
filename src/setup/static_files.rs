f28e113a22d1f224aa82f1282e776f781dde6aa4
P34RL <undeadgamer279@gmail.com>
2023-09-16 20:52:56 -0400

    extra macro

--- src/setup/static_files.rs
+++ src/setup/static_files.rs
@@ -4,6 +4,14 @@ macro_rules! include_static {
     ($file:expr) => {
         include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/static/", $file))
     };
+
+    ($file:expr, $raw_type:ident) => {
+        $raw_type(include_str!(concat!(
+            env!("CARGO_MANIFEST_DIR"),
+            "/static/",
+            $file
+        )))
+    };
 }
 
 #[get("/favicon.ico")]
@@ -12,13 +20,13 @@ fn icon() -> &'static [u8] {
 }
 
 #[get("/style.css")]
-fn css() -> RawCss<&'static [u8]> {
-    RawCss(include_static!("style.css"))
+fn css() -> RawCss<&'static str> {
+    include_static!("style.css", RawCss)
 }
 
 #[get("/htmx.min.js")]
-fn js() -> RawJavaScript<&'static [u8]> {
-    RawJavaScript(include_static!("htmx.min.js"))
+fn js() -> RawJavaScript<&'static str> {
+    include_static!("htmx.min.js", RawJavaScript)
 }
 
 pub fn get_static_routes() -> Vec<rocket::Route> {
