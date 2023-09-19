4e9b0f058116662efc8afedd8176f2837e12ee4f
P34RL <undeadgamer279@gmail.com>
2023-09-19 08:57:06 -0400

    simplified code

--- src/api/repeatkey.rs
+++ src/api/repeatkey.rs
@@ -15,28 +15,6 @@ pub struct Repeat<'r> {
     interval_seconds: u64,
 }
 
-fn repeat_key(state: Arc<RwLock<KeyState>>, interval_seconds: u64, key: enigo::Key) {
-    let mut enigo = Enigo::new();
-    while *state.read().expect("read state") == KeyState::Repeating(key, interval_seconds) {
-        enigo.key_click(key);
-
-        thread::sleep(Duration::from_secs(interval_seconds));
-    }
-}
-
-fn handle_state(
-    key_state: &RwLock<KeyState>,
-    key: enigo::Key,
-    interval_seconds: u64,
-) -> Result<(), ()> {
-    if *key_state.read().expect("read state") == KeyState::Repeating(key, interval_seconds) {
-        return Err(());
-    }
-
-    *key_state.write().expect("write to state") = KeyState::Repeating(key, interval_seconds);
-    Ok(())
-}
-
 #[get("/repeat/start?<query..>")]
 pub fn start_repeating(key_state: &State<Arc<RwLock<KeyState>>>, query: Repeat) -> String {
     let Repeat {
@@ -46,13 +24,22 @@ pub fn start_repeating(key_state: &State<Arc<RwLock<KeyState>>>, query: Repeat) 
 
     let key = enigo::Key::Layout(letter.chars().next().expect("get letter"));
 
-    if handle_state(key_state, key, interval_seconds).is_err() {
+    if *key_state.read().expect("read state") == KeyState::Repeating(key, interval_seconds) {
         return format!("Already Repeating {}", letter);
+    } else {
+        *key_state.write().expect("write to state") = KeyState::Repeating(key, interval_seconds);
     }
 
     let state = Arc::clone(key_state.inner());
 
-    thread::spawn(move || repeat_key(state, interval_seconds, key));
+    thread::spawn(move || {
+        let mut enigo = Enigo::new();
+        while *state.read().expect("read state") == KeyState::Repeating(key, interval_seconds) {
+            enigo.key_click(key);
+
+            thread::sleep(Duration::from_secs(interval_seconds));
+        }
+    });
 
     String::from("Successfully Started Repeating")
 }
